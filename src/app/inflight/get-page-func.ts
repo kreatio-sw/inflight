3d548295848e90917f87facdc8643bd76a3feee0
Deepak Kumar <deepak@kreatio.com>
2019-07-07 15:47:04 +0530

    Getting towards lint cleanliness: Interface to type

--- src/app/inflight/get-page-func.ts
+++ src/app/inflight/get-page-func.ts
@@ -10,14 +10,5 @@ import {PagedResults} from '../interfaces/paged-results';
  * This will typically be Angular HTTP `get` call, followed by `map`.
  * To add timeout and retry, use standard RxJS techniques at this stage.
  */
-export interface GetPageFunc {
-  /**
-   * The function will receive `pageNumber` and `perPage`.
-   * It needs to return an `Observable` that will yield {@link PagedResults}.
-   * This will typically be Angular HTTP `get` call, followed by `map`.
-   * To add timeout and retry, use standard RxJS techniques at this stage.
-   *
-   */
-  (page: number, perPage: number): Observable<PagedResults>;
-}
+export type GetPageFunc = (page: number, perPage: number) => Observable<PagedResults>;
 
